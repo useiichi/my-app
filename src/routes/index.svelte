8f173b759372f94ff787a53410f87f0f45b460a2
Sss Uuu <username@example.com>
2022-07-28 14:45:18 +0900

    mkdwn

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
+<script lang='ts'>
+	import Markdown from 'svelte-exmarkdown';
+	let md = '# Hello world!';
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<textarea bind:value={md} />
+<Markdown {md} />
